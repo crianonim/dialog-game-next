8b97c6c56a73f988c151044cf62b77a9889c2e9a
Jan Skowronski <crianonim@gmail.com>
2023-08-29 19:06:33 +0100

    setup lang only once

--- src/app/screept/editor.tsx
+++ src/app/screept/editor.tsx
@@ -8,6 +8,73 @@ import Editor, {
 } from "@monaco-editor/react";
 import { Position, editor } from "monaco-editor";
 import * as S from "@/screept-lang";
+
+function setUpLanguage(monaco: Monaco, environment: S.Environment) {
+  const keywords = ["PRINT", "IF", "THEN", "RND", "FUNC", "PROC", "RUN"];
+
+  monaco.languages.register({ id: "screept" });
+  monaco.languages.registerCompletionItemProvider("screept", {
+    provideCompletionItems: (model, position: Position) => {
+      const suggestions = [
+        ...keywords.map((k) => {
+          return {
+            label: k,
+            insertText: k,
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            range: {
+              startLineNumber: position.lineNumber,
+              endLineNumber: position.lineNumber,
+              startColumn: model.getWordUntilPosition(position).startColumn,
+              endColumn: model.getWordUntilPosition(position).endColumn,
+            },
+          };
+        }),
+        ...Object.keys(environment.vars).map((k) => ({
+          label: k,
+          insertText: k,
+          kind: monaco.languages.CompletionItemKind.Variable,
+          range: {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn: model.getWordUntilPosition(position).startColumn,
+            endColumn: model.getWordUntilPosition(position).endColumn,
+          },
+        })),
+        ...Object.keys(environment.procedures).map((k) => ({
+          label: k,
+          insertText: k,
+          kind: monaco.languages.CompletionItemKind.Function,
+          range: {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn: model.getWordUntilPosition(position).startColumn,
+            endColumn: model.getWordUntilPosition(position).endColumn,
+          },
+        })),
+      ];
+      //   console.log({ suggestions });
+      return { suggestions };
+    },
+  });
+  monaco.languages.setMonarchTokensProvider("screept", {
+    keywords,
+    tokenizer: {
+      root: [
+        [
+          /@?[a-zA-Z][\w$]*/,
+          {
+            cases: {
+              "@keywords": "keyword",
+              "@default": "variable",
+            },
+          },
+        ],
+        [/".*?"/, "string"],
+      ],
+    },
+  });
+}
+
 type ScreeptEditorProps = {
   onChange: OnChange;
   initialValue: string;
@@ -27,89 +94,94 @@ function ScreeptEditor({
     m: Monaco
   ) {
     editorRef.current = editor;
+    if (m !== monaco) {
+      console.log("Brand new Monaco!", m, monaco);
+      setUpLanguage(m, environment);
+    }
   }
   /// TODO Show errors
-  if (editorRef.current && monaco && editorRef.current.getModel()) {
-    const model = editorRef.current.getModel();
-    if (model)
-      monaco.editor.setModelMarkers(model, "owner", [
-        {
-          severity: monaco.MarkerSeverity.Error,
-          startColumn: 1,
-          endColumn: model.getFullModelRange().endColumn,
-          startLineNumber: 1,
-          endLineNumber: model.getLineCount(),
-          message: "Bad screept",
-        },
-      ]);
-  }
+  //   if (editorRef.current && monaco && editorRef.current.getModel()) {
+  //     const model = editorRef.current.getModel();
+  //     if (model)
+  //       monaco.editor.setModelMarkers(model, "owner", [
+  //         {
+  //           severity: monaco.MarkerSeverity.Error,
+  //           startColumn: 1,
+  //           endColumn: model.getFullModelRange().endColumn,
+  //           startLineNumber: 1,
+  //           endLineNumber: model.getLineCount(),
+  //           message: "Bad screept",
+  //         },
+  //       ]);
+  //   }
 
-  useEffect(() => {
-    const keywords = ["PRINT", "IF", "THEN", "RND", "FUNC", "PROC", "RUN"];
-    if (monaco) {
-      monaco.languages.register({ id: "screept" });
-      monaco.languages.registerCompletionItemProvider("screept", {
-        provideCompletionItems: (model, position: Position) => {
-          const suggestions = [
-            ...keywords.map((k) => {
-              return {
-                label: k,
-                insertText: k,
-                kind: monaco.languages.CompletionItemKind.Keyword,
-                range: {
-                  startLineNumber: position.lineNumber,
-                  endLineNumber: position.lineNumber,
-                  startColumn: model.getWordUntilPosition(position).startColumn,
-                  endColumn: model.getWordUntilPosition(position).endColumn,
-                },
-              };
-            }),
-            ...Object.keys(environment.vars).map((k) => ({
-              label: k,
-              insertText: k,
-              kind: monaco.languages.CompletionItemKind.Variable,
-              range: {
-                startLineNumber: position.lineNumber,
-                endLineNumber: position.lineNumber,
-                startColumn: model.getWordUntilPosition(position).startColumn,
-                endColumn: model.getWordUntilPosition(position).endColumn,
-              },
-            })),
-            ...Object.keys(environment.procedures).map((k) => ({
-              label: k,
-              insertText: k,
-              kind: monaco.languages.CompletionItemKind.Function,
-              range: {
-                startLineNumber: position.lineNumber,
-                endLineNumber: position.lineNumber,
-                startColumn: model.getWordUntilPosition(position).startColumn,
-                endColumn: model.getWordUntilPosition(position).endColumn,
-              },
-            })),
-          ];
-          return { suggestions };
-        },
-      });
-      monaco.languages.setMonarchTokensProvider("screept", {
-        keywords,
-        tokenizer: {
-          root: [
-            [
-              /@?[a-zA-Z][\w$]*/,
-              {
-                cases: {
-                  "@keywords": "keyword",
-                  "@default": "variable",
-                },
-              },
-            ],
-            [/".*?"/, "string"],
-          ],
-        },
-      });
-      console.log("here is the monaco instance:", monaco);
-    }
-  }, [monaco]);
+  //   useEffect(() => {
+  //     const keywords = ["PRINT", "IF", "THEN", "RND", "FUNC", "PROC", "RUN"];
+  //     if (monaco) {
+  //       monaco.languages.register({ id: "screept" });
+  //       monaco.languages.registerCompletionItemProvider("screept", {
+  //         provideCompletionItems: (model, position: Position) => {
+  //           const suggestions = [
+  //             ...keywords.map((k) => {
+  //               return {
+  //                 label: k,
+  //                 insertText: k,
+  //                 kind: monaco.languages.CompletionItemKind.Keyword,
+  //                 range: {
+  //                   startLineNumber: position.lineNumber,
+  //                   endLineNumber: position.lineNumber,
+  //                   startColumn: model.getWordUntilPosition(position).startColumn,
+  //                   endColumn: model.getWordUntilPosition(position).endColumn,
+  //                 },
+  //               };
+  //             }),
+  //             ...Object.keys(environment.vars).map((k) => ({
+  //               label: k,
+  //               insertText: k,
+  //               kind: monaco.languages.CompletionItemKind.Variable,
+  //               range: {
+  //                 startLineNumber: position.lineNumber,
+  //                 endLineNumber: position.lineNumber,
+  //                 startColumn: model.getWordUntilPosition(position).startColumn,
+  //                 endColumn: model.getWordUntilPosition(position).endColumn,
+  //               },
+  //             })),
+  //             ...Object.keys(environment.procedures).map((k) => ({
+  //               label: k,
+  //               insertText: k,
+  //               kind: monaco.languages.CompletionItemKind.Function,
+  //               range: {
+  //                 startLineNumber: position.lineNumber,
+  //                 endLineNumber: position.lineNumber,
+  //                 startColumn: model.getWordUntilPosition(position).startColumn,
+  //                 endColumn: model.getWordUntilPosition(position).endColumn,
+  //               },
+  //             })),
+  //           ];
+  //           console.log({ suggestions });
+  //           return { suggestions };
+  //         },
+  //       });
+  //       monaco.languages.setMonarchTokensProvider("screept", {
+  //         keywords,
+  //         tokenizer: {
+  //           root: [
+  //             [
+  //               /@?[a-zA-Z][\w$]*/,
+  //               {
+  //                 cases: {
+  //                   "@keywords": "keyword",
+  //                   "@default": "variable",
+  //                 },
+  //               },
+  //             ],
+  //             [/".*?"/, "string"],
+  //           ],
+  //         },
+  //       });
+  //       console.log("here is the monaco instance:", monaco);
+  //     }
+  //   }, [monaco]);
   const options: editor.IStandaloneEditorConstructionOptions = {
     minimap: { enabled: false },
     wordWrap: "wordWrapColumn",
