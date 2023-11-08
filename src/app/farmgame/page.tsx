dc5eb23351f25412b3e4030c640c7ad26c032cda
Jan Skowronski <crianonim@gmail.com>
2023-11-08 08:25:41 +0000

    farm game

---
+++ src/app/farmgame/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+function farmgame() {
+  return <div>Farm Game</div>;
+}
+
+export default farmgame;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-4">
+    <main className="flex min-h-screen p-4 gap-4">
       <Link href="/dialoggame">Game</Link>
+      <Link href="/farmgame">Farm Game</Link>
     </main>
   );
 }
